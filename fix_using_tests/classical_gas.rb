bb4cb2744e0a6094ec78c8e2d9c32d01a56eba57
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-17 19:11:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -1,3 +1,4 @@
+require "pry"
 class GasStation
 
   # Remember which methods this "magic command", "attr_reader" creates.
@@ -16,4 +17,5 @@ end
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
+
 binding.pry
